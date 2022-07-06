488f432ef5c48bc78fec52d365afe95c55f09b18
Olesya <olesya.leonovich@gmail.com>
2022-07-06 20:07:23 +0300

    feat: change user hook

--- skypro-react/src/homework_9/LoginPassword/useInput.jsx
+++ skypro-react/src/homework_9/LoginPassword/useInput.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react';
 
 const validEmail = new RegExp(/^[\w]{1}[\w-.]*@[\w-]+\.[a-z]{2,4}$/i);
-const validPassword = 6;
+const validPasswordLength = 6;
 
 const useInput = (value) => {
   const [inputValue, setInputValue] = useState(value);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const onBlur = (event) => {
-    const { name, value } = event.target;
-
-    if (name === 'password' && value.length < validPassword) {
+  const validation = (name, value) => {
+    if (name === 'password' && value.length < validPasswordLength) {
       setErrorMessage('Password should be at least 6 characters');
     } else {
       setErrorMessage('');
@@ -19,7 +17,11 @@ const useInput = (value) => {
     if (name === 'email' && !value.match(validEmail)) {
       setErrorMessage('Email should be valid');
     }
+  };
 
+  const onBlur = (event) => {
+    const { name, value } = event.target;
+    validation(name, value);
     !inputValue && setErrorMessage(`Please, fill ${name} field`);
   };
 
